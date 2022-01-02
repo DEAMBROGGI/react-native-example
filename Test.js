4b9951f6cfb9fea815f039c41eb49ecca6f71b35
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-01-02 14:59:33 -0300

    testing commit

--- Test.js
+++ Test.js
@@ -38,7 +38,7 @@ export default function Test() {
       <Separator/>
       <TextInput
         style={{height: 40}}
-        placeholder="Type here to translate the text test!"
+        placeholder="Type here to translate the text test TESTING!"
         onChangeText={texto => dispatch({type: actionTypes.TEXT,
                                         text:texto})}
        
